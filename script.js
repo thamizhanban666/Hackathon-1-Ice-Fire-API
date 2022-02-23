2d99271c52daea947eb29a21242b331ef7ee15d5
thamizhanban666 <thamizhanban666@gmail.com>
2022-02-23 12:38:16 +0530

    hackathon-1

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ const myFetchChar = async (c) => {     //console.log(`${c}`);
   return await response.json();
 };
 
+let showCharacter = (c) => {
+   let mBody = document.getElementById("modal-body");
+   mBody.innerText = c.join(" ")
+}
+
 let parentDiv = document.createElement("div");
 parentDiv.classList.add("row","text-center","mx-2","my-2","text-light","bg-primary");
 document.body.appendChild(parentDiv);
 
 let childDiv = document.createElement("div");
-childDiv.classList.add("row","text-center","mx-2","my-2","text-dark");
+childDiv.classList.add("row","text-center","x-2","my-2","text-dark");
 document.body.appendChild(childDiv)
 
 let div1 = document.createElement("div");
@@ -27,6 +32,11 @@ let div2 = document.createElement("div");
 div2.classList.add("col-2","fw-bold","border","border-dark","py-4","fs-5")
 div2.innerText = "NAME";
 parentDiv.appendChild(div2);
+
+let div8 = document.createElement("div");
+div8.classList.add("col-1","fw-bold","border","border-dark","py-4","fs-5")
+div8.innerText = "Img";
+parentDiv.appendChild(div8);
 
 let div3 = document.createElement("div");
 div3.classList.add("col-2","fw-bold","border","border-dark","py-4","fs-5")
@@ -49,89 +59,66 @@ div6.innerHTML = "PUBLISHER & RELEASED DATE";
 parentDiv.appendChild(div6);
 
 let div7 = document.createElement("div");
-div7.classList.add("col-2","fw-bold","border","border-dark","py-4","fs-5")
+div7.classList.add("col-1","fw-bold","border","border-dark","py-4","px-0","fs-6")
 div7.innerText = "CHARACTERS";
 parentDiv.appendChild(div7);
 
    myFetchall()
    .then((arrobj) => {
-      arrobj.map((e,i) => { 
+      arrobj.map((e, i) => { 
+         let cDiv = document.createElement("div");
+         cDiv.classList.add("row","border","border-primary","mx-1","my-2");
+         childDiv.appendChild(cDiv)
+
          let d1 = document.createElement("div");
-         d1.classList.add("col-1", "fw-bold", "border", "border-dark", "py-4", "fs-5");
+         d1.classList.add("col-1", "fw-bold", "my-auto", "fs-5");
          d1.innerText = i+1;
-         childDiv.appendChild(d1);
+         cDiv.appendChild(d1);
 
          let d2 = document.createElement("div");
-         d2.classList.add("col-2", "fw-bold", "border", "border-dark", "py-4", "fs-6");
+         d2.classList.add("col-2", "fw-bold", "my-auto", "fs-6");
          d2.innerText = e.name;
-         childDiv.appendChild(d2);
+         cDiv.appendChild(d2);
+
+         let d8 = document.createElement("div");
+         d8.classList.add("col-1", "fw-bold","my-1", "fs-6");
+         let img = document.createElement("img");
+         img.setAttribute("src", `./img${i+1}.jpg`);
+         img.setAttribute("style","width:100%")
+         d8.appendChild(img);
+         cDiv.appendChild(d8);
 
          let d3 = document.createElement("div");
-         d3.classList.add("col-2", "fw-bold", "border", "border-dark", "py-4", "fs-6");
+         d3.classList.add("col-2", "fw-bold", "my-auto", "fs-6");
          d3.innerText = e.isbn;
-         childDiv.appendChild(d3);
+         cDiv.appendChild(d3);
 
          let d4 = document.createElement("div");
-         d4.classList.add("col-1", "fw-bold", "border", "border-dark", "py-4", "fs-6");
+         d4.classList.add("col-1", "fw-bold", "my-auto", "fs-6");
          d4.innerText = e.numberOfPages;
-         childDiv.appendChild(d4);
+         cDiv.appendChild(d4);
 
          let d5 = document.createElement("div");
-         d5.classList.add("col-2", "fw-bold", "border", "border-dark", "py-4", "fs-6");
+         d5.classList.add("col-2", "fw-bold", "my-auto", "fs-6");
          d5.innerText = e.authors;
-         childDiv.appendChild(d5);
+         cDiv.appendChild(d5);
 
          let d6 = document.createElement("div");
-         d6.classList.add("col-2", "fw-bold", "border", "border-dark", "py-3", "fs-6");
+         d6.classList.add("col-2", "fw-bold", "my-auto", "fs-6");
          d6.innerHTML = `${e.publisher} <br> ${e.released.slice(0,10)}`
-         childDiv.appendChild(d6);
+         cDiv.appendChild(d6);
 
-         let d7 = document.createElement("div");
-         d7.classList.add("col-2", "fw-bold", "border", "border-dark", "py-4", "fs-6");
+         let d7 = document.createElement("button");
+         d7.classList.add("col-1", "fw-bold","px-auto", "my-auto", "fs-6","btn","btn-sm");
+         d7.setAttribute("style", "width:4rem");
+         d7.setAttribute("data-bs-toggle","modal")
+         d7.setAttribute("data-bs-target","#cModal")
+         d7.innerText = "show"         
+         cDiv.appendChild(d7);
 
-        
-         // let dummy = async () => {
-
-            let str;
-            let charArr = [];
-           
-            // e["characters"].map((c) => {
-            
-
-            for (let j = 0; j < 2; j++) {
-               if (e.characters[j] == undefined) { continue; }
-               else {
-                  // console.log(e.characters[j]);
-                  myFetchChar(e.characters[j])
-                     .then((charObj) => {
-                        // if (charObj.name == "") { }
-                        // else {
-                        // console.log(charObj.name)
-                        str = charObj.name;
-                        charArr.push(str)
-                        // }
-                     })
-               }
-            }
-         
-            // })
-            // debugger;
-            // console.log(str)
-            // console.log(charArr);
-            // let html = "";
-         
-            //    for(var x=0; x < charArr.length; x++){
-            //       html += charArr[x];   // Populate string with array values
-            //    }
-            //    // document.body.innerHTML = html; // Append string only once
-            // return await charArr;
-        
-         // }
-         console.log(str)
-         console.log(charArr)
-         d7.innerText = charArr;
-         // d7.textContent = str
-         childDiv.appendChild(d7);
+         d7.addEventListener("click", () => {
+            showCharacter(e.characters)
+         })
            
       })
       
@@ -142,22 +129,15 @@ let searchBtn = document.getElementById("searchBtn");
 let searchWikiBtn = document.getElementById("searchWikiBtn")
 let link = "https://en.wikipedia.org/wiki/";
 
-// searchBtn.addEventListener("click", () => {
 onchange = () => {
    if (searchBar.value.length > 0) {
       window.find(searchBar.value)
    }
 }
-// })
 
 searchWikiBtn.addEventListener("click", () => {
     if (searchBar.value.length > 0) {
       location.href = link + searchBar.value;
    }
 })
-
-
-
-
-
 
